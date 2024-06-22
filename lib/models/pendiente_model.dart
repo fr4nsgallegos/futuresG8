2f63cfb96082f74d5dd5f60ce3b4e8d5d4207396
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-06-22 13:01:56 -0500

    interaction with future

--- lib/models/pendiente_model.dart
+++ lib/models/pendiente_model.dart
@@ -2,10 +2,12 @@ class PendienteModel {
   int id;
   String titulo;
   String description;
+  String imageUrl;
 
   PendienteModel({
     required this.id,
     required this.titulo,
     required this.description,
+    required this.imageUrl,
   });
 }
--- lib/pages/Perfil_page.dart
+++ lib/pages/Perfil_page.dart
@@ -2,90 +2,113 @@ import 'package:flutter/material.dart';
 import 'package:futuresg8/models/pendiente_model.dart';
 import 'package:futuresg8/models/profile_model.dart';
 import 'package:futuresg8/models/social_model.dart';
+import 'package:futuresg8/services/service_data.dart';
 
-class PerfilPage extends StatelessWidget {
-  ProfileModel persona1 = ProfileModel(
-    id: 1,
-    fullName: "Juan Manuek",
-    profesion: "Developer",
-    image:
-        "https://img.freepik.com/foto-gratis/apuesto-hombre-caucasico-ropa-informal-gafas-sonrisa-alegre-fresca-cara-persona-suerte_839833-12772.jpg",
-    information:
-        "s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown p",
-    socialList: [
-      SocialModel(titulo: "Articles", value: 43),
-      SocialModel(titulo: "Followers", value: 234),
-      SocialModel(titulo: "Rating", value: 6.3),
-    ],
-    pendientesList: [
-      PendienteModel(
-          id: 1,
-          titulo: "Ir de compras al super",
-          description: "No te olvides de llevar la lista"),
-      PendienteModel(
-          id: 2,
-          titulo: "Lllevar las cosas a la casa del trabajo",
-          description: "Son cosas delicadas, tener cuidado"),
-      PendienteModel(
-          id: 3,
-          titulo: "Consiguir comida para las mascotas",
-          description: "Debe ser bajo en grasas"),
-    ],
-  );
+class PerfilPage extends StatefulWidget {
+  @override
+  State<PerfilPage> createState() => _PerfilPageState();
+}
+
+class _PerfilPageState extends State<PerfilPage> {
+  late ProfileModel persona1;
+  ServiceData serviceData = ServiceData();
+
+  Future<void> getData() async {
+    persona1 = await serviceData.getProfile();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    // getData();
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Container(
-                padding: EdgeInsets.all(16),
-                width: double.infinity,
-                height: 200,
-                color: Colors.red,
-                child: Row(
-                  children: [
-                    Image.network(
-                      persona1.image,
-                      fit: BoxFit.cover,
-                      height: 90,
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
+            padding: const EdgeInsets.all(16.0),
+            child: FutureBuilder(
+                future: serviceData.getSpecificProfile(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    // List<ProfileModel> profilesList = snapshot.data!;
+                    persona1 = snapshot.data!;
+                    // persona1 = profilesList[1];
+                    return Column(
                       children: [
-                        Text("${persona1.fullName} - ${persona1.profesion}"),
-                        Row(
-                          children: [
-                            ...persona1.socialList
-                                .map(
-                                  (e) => Padding(
-                                    padding: const EdgeInsets.all(10.0),
-                                    child: Column(
-                                      children: [
-                                        Text(e.titulo),
-                                        SizedBox(
-                                          height: 8,
-                                        ),
-                                        Text(e.value.toString()),
-                                      ],
-                                    ),
+                        Container(
+                          padding: EdgeInsets.all(16),
+                          width: double.infinity,
+                          height: 200,
+                          color: Colors.red,
+                          child: Row(
+                            children: [
+                              Image.network(
+                                persona1.image,
+                                fit: BoxFit.cover,
+                                height: 90,
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Text(
+                                      "${persona1.fullName} - ${persona1.profesion}"),
+                                  Row(
+                                    children: [
+                                      ...persona1.socialList
+                                          .map(
+                                            (e) => Padding(
+                                              padding:
+                                                  const EdgeInsets.all(10.0),
+                                              child: Column(
+                                                children: [
+                                                  Text(e.titulo),
+                                                  SizedBox(
+                                                    height: 8,
+                                                  ),
+                                                  Text(e.value.toString()),
+                                                ],
+                                              ),
+                                            ),
+                                          )
+                                          .toList(),
+                                      // Column(children: [Text(persona1.socialList)],)
+                                    ],
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                        Text("INFORMACION"),
+                        Text(persona1.information),
+                        ...persona1.pendientesList
+                            .map(
+                              (e) => Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: ListTile(
+                                  leading: Image.network(e.imageUrl),
+                                  title: Text(e.titulo),
+                                  subtitle: Text(e.description),
+                                  trailing: Icon(
+                                    Icons.check_circle,
+                                    color: Colors.green,
                                   ),
-                                )
-                                .toList(),
-                            // Column(children: [Text(persona1.socialList)],)
-                          ],
-                        )
+                                ),
+                              ),
+                            )
+                            .toList(),
                       ],
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
+                    );
+                  }
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                })),
       ),
     );
   }
