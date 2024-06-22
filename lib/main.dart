27a1300958152988c05d3d5a3e0c3cf5872ca914
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-06-22 11:27:32 -0500

    propiedeades de asyncsnapshot

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:futuresg8/pages/home_page.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: HomePage(),
-  ));
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+    ),
+  );
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,85 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String title = "Sin título";
+
+  Future<String> getTitle() {
+    return Future.delayed(Duration(seconds: 3), () {
+      return "Mi Planta de Naranja Lima";
+    });
+  }
+
+  Future<int> getNumber() {
+    return Future.delayed(Duration(seconds: 5), () {
+      return 168;
+    });
+  }
+
+  //SEGUNDA FORMA DE DESEVOLVER FUTURE
+  setTitle() async {
+    title = await getTitle();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    //PRIMERA FORMA DE DESENVOLVER FUTURE
+    // getTitle().then((value) {
+    //   title = value;
+    //   print(title);
+    //   setState(() {});
+    // });
+
+    //SEGUNDA FORMA DE DESEVOLVER FUTURE
+    setTitle();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Futures"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                title,
+                style: TextStyle(fontSize: 30),
+              ),
+              Padding(
+                padding: EdgeInsets.all(48),
+                child: FutureBuilder(
+                  future: getNumber(),
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    print("------------------------------");
+                    print("SNAP: $snapshot");
+                    print("Estado de conexión: ${snapshot.connectionState}");
+                    print("¿Tiene data?: ${snapshot.hasData}");
+                    print("data: ${snapshot.data}");
+                    print("tiene error: ${snapshot.hasError}");
+                    print("hasshcode: ${snapshot.hashCode}");
+                    print("------------------------------");
+                    return Text(
+                      snapshot.data == null
+                          ? "esperando"
+                          : snapshot.data.toString(),
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
